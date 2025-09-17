238fc570c782de4da5a5d426e83f7610eaa1781f
Bart Prokop <bprokop@visa.com>
2025-09-17 14:13:19 +0000

    Adding installation of starship

---
+++ gcs/tools.sh
@@ -0,0 +1,4 @@
+# apt install starship
+wget https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-musl.tar.gz
+tar xzf starship-x86_64-unknown-linux-musl.tar.gz -C ~/bin
+rm starship-x86_64-unknown-linux-musl.tar.gz
