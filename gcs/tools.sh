e0f0309d9b0c6cb9d43f4382ebef82a3df250a6d
Bart Prokop <bprokop@visa.com>
2025-09-19 21:12:30 +0000

    Adding zsh startup file backup

--- gcs/tools.sh
+++ gcs/tools.sh
@@ -1,10 +1,12 @@
+mkdir -p ~/bin
+
 # apt install starship
 wget https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-musl.tar.gz
-tar xzf starship-x86_64-unknown-linux-musl.tar.gz -C ~/bin
+tar xzfv starship-x86_64-unknown-linux-musl.tar.gz -C ~/bin
 rm starship-x86_64-unknown-linux-musl.tar.gz
 
 # 103  echo 'eval "$(starship init bash)"'
 # 104  echo 'eval "$(starship init bash)"' >> .bashrc 
 
-curl https://raw.githubusercontent.com/dylanaraps/neofetch/refs/heads/master/neofetch > ~/bin/neofetch
-chmod +x ~/bin/neofetch
+# curl https://raw.githubusercontent.com/dylanaraps/neofetch/refs/heads/master/neofetch > ~/bin/neofetch
+# chmod +x ~/bin/neofetch
